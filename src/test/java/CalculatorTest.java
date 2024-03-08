880518460f04fac1b022fede045addc99ac62eb1
Ubuntu <ubuntu@ip-172-31-95-5.ec2.internal>
2024-03-08 11:43:57 +0000

    d

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+}
+
