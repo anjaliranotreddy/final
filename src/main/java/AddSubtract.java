8d9885562db77b44ab06e5f61b831195b6ac8f77
Ubuntu <ubuntu@ip-172-31-90-207.ec2.internal>
2024-03-08 10:07:18 +0000

    done1

---
+++ src/main/java/AddSubtract.java
@@ -0,0 +1,15 @@
+public class AddSubtract {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        // Addition
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+
+        // Subtraction
+        int diff = num1 - num2;
+        System.out.println("Difference: " + diff);
+    }
+}
+
